098c8bdbb9ea5a82f1ccbeea5ab6a1a88069b223
leoliu <leoliu@yunify.com>
2020-04-08 11:49:38 +0800

    fix: Fix hotjar scripts

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -7,7 +7,7 @@ import '../i18n'
 import '../assets/fonts/ProximaNova/stylesheet.css'
 import './index.css'
 
-const Layout = ({ children, data }) => (
+const Layout = ({ children, data, locale }) => (
   <div>
     <Helmet
       title={data.site.siteMetadata.title}
@@ -20,6 +20,17 @@ const Layout = ({ children, data }) => (
       ]}
     />
     {children}
+    {locale && (
+      <script
+        type="text/javascript"
+        dangerouslySetInnerHTML={{
+          __html:
+            locale.indexOf('zh') !== -1
+              ? `(function(h,o,t,j,a,r){ h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)}; h._hjSettings={hjid:1749426,hjsv:6}; a=o.getElementsByTagName('head')[0]; r=o.createElement('script');r.async=1; r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv; a.appendChild(r); })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`
+              : `(function(h,o,t,j,a,r){ h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)}; h._hjSettings={hjid:1756364,hjsv:6}; a=o.getElementsByTagName('head')[0]; r=o.createElement('script');r.async=1; r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv; a.appendChild(r); })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv='); `,
+        }}
+      />
+    )}
   </div>
 )
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -82,7 +82,7 @@ class IndexPage extends React.Component {
     }
 
     return (
-      <Layout data={this.props.data}>
+      <Layout data={this.props.data} locale={locale}>
         <div>
           <Header {...this.props} pathPrefix={pathPrefix} />
           <Content
